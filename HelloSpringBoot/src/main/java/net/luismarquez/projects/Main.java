375d821f0f0c94cb3501867647982d9ded11b520
luismarquez96c <luismarquez96c@gmail.com>
2023-11-04 15:11:49 -0700

    Logro: Comprender qué es spring y crear nuestro primer proyecto spring boot.

---
+++ HelloSpringBoot/src/main/java/net/luismarquez/projects/Main.java
@@ -0,0 +1,22 @@
+package net.luismarquez.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Main.class, args);
+
+    }
+
+    @RequestMapping("/example")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+}
